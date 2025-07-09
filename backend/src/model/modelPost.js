be4be9d1eda00ca59064f056f02481e021b78541
DhirajKarangale <dakarangale02@gmail.com>
2025-07-09 19:22:20 +0530

    Posts adding to users

--- backend/src/model/modelPost.js
+++ backend/src/model/modelPost.js
@@ -1,10 +1,20 @@
 const db = require('./db');
 
 async function Post(userId, postTitle, postBody, mediaURL, category) {
-    await db.query(
-        'INSERT INTO posts (user_id, post_title, post_body, media_url, category) VALUES ($1, $2, $3, $4, $5)',
+    const res = await db.query(
+        `INSERT INTO posts (user_id, post_title, post_body, media_url, category) 
+         VALUES ($1, $2, $3, $4, $5)
+         RETURNING id;`,
         [userId, postTitle, postBody, mediaURL, category]
     );
+
+    const postId = res.rows[0].id;
+
+    await db.query(`
+        UPDATE users SET posts = COALESCE(posts, '{}') || $1
+        WHERE id = $2;`, [[postId], userId]);
+
+    console.log();
 }
 
 module.exports = { Post };
