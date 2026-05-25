772c4eaf8e8fe6454d47f3ee4e4a65f9d8958748
DhirajKarangale <dakarangale02@gmail.com>
2026-05-25 16:30:00 +0530

    Update user route in Navbar and add userOwn route in Routes

--- frontend/src/utils/Routes.ts
+++ frontend/src/utils/Routes.ts
@@ -3,6 +3,7 @@ export const routeFeed = '/feed';
 export const routePopular = '/';
 export const routeCustomizeFeed = '/customizefeed';
 export const routeAddPost = '/addpost';
+export const routeUserOwn = '/user';
 export const routeUser = '/user/:userId';
 export const routeAbout = '/about';
 export const routeMessages = '/messages';
